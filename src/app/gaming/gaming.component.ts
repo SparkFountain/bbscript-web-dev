95d0b28f941158b0f7c6d69e161c736c47707518
Stefan Behrendt <stefan.behrendt@sparkfountain.de>
2020-01-26 23:57:44 +0100

    work in progress

--- src/app/gaming/gaming.component.ts
+++ src/app/gaming/gaming.component.ts
@@ -8,6 +8,8 @@ import 'brace/theme/github';
 import 'brace/theme/cobalt';
 import 'brace/theme/terminal';
 import '../../assets/ace/mode-bbscript';
+import { Observable, Subscriber, concat, of } from 'rxjs';
+import { tap, withLatestFrom, map, switchMap } from 'rxjs/operators';
 
 
 @Component({
@@ -43,7 +45,47 @@ export class GamingComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.ace.instance = this.directiveRef.ace();
+    this.testingSection();
   }
+
+  /** BEGIN OF TESTING SECTION */
+  testingSection() {
+    console.info('Testing section');
+
+    let variable = { value: 0 };
+    const obsArray$: Observable<any>[] = [];
+
+    const obs1$ = new Observable<void>((observer: Subscriber<void>) => {
+      variable.value = 42;
+      observer.next();
+      observer.complete();
+    });
+    obsArray$.push(obs1$);
+
+    // DOES NOT WORK
+    // obsArray$.push(this.addValues$(variable, 58).pipe(
+    //   tap((result: number) => console.info('TEST RESULT:', result))
+    // ));
+
+    // WORKS
+    const params: [{ value: number }, number] = [variable, 58];
+    obsArray$.push(of(null).pipe(
+      switchMap(() => this.addValues$(...params)),
+      tap((result) => console.info('TEST RESULT:', result))
+    ));
+
+    // obsArray$.push(this.addValues$(variable, 58).pipe(
+    //   tap((result: number) => console.info('TEST RESULT:', result))
+    // ));
+
+    concat(...obsArray$).subscribe();
+  }
+
+  addValues$(x: { value: number }, y: number) {
+    return of(x.value + y);
+  }
+
+  /** END OF TESTING SECTION */
 
   play() {
     this.section = 'game';
